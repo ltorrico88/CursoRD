a2ba5e35b6d661554d97c9b2c559affa2e5900e1
Leonardo Torrico <leo.f.t@hotmail.com>
2022-09-18 12:36:11 -0300

    TP semana 5

---
+++ Clase9/02-variables.js
@@ -0,0 +1,20 @@
+// VARIABLES
+// Una variable es un contenedor de informacion
+
+var pais = "España";
+var continente = "Europa";
+var antiguedad = "2021";
+var pais_y_continente_y_antiguedad = pais + " " + continente + " " + antiguedad;
+
+var pais2 = "México";
+var continente2 = "Latinoamerica";
+var pais_y_continente2 = pais2 + " es parte de " + continente2;  
+
+var pais3 = "Japon"
+var continente3 = "Asiatico"
+var pais_y_continente3 = pais3 + " pertenece al continente " + continente3;
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente_y_antiguedad);
+alert(pais_y_continente2);
+alert(pais_y_continente3);
